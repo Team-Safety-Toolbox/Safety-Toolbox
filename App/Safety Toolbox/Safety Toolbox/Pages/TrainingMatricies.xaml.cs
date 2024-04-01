95c328513b98d1eacd80ba95c6dfacdfd7aeddff
Mikayla Peterson <98986881+mikaylapeterson@users.noreply.github.com>
2024-04-01 15:31:40 -0600

    empty training matrices can be generated
    still working on the case where just the employee is selected and when both are selected

--- App/Safety Toolbox/Safety Toolbox/Pages/TrainingMatricies.xaml.cs
+++ App/Safety Toolbox/Safety Toolbox/Pages/TrainingMatricies.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.Data.SqlClient;
+using System.Xml;
 
 namespace Safety_Toolbox;
 
@@ -6,6 +7,8 @@ public partial class TrainingMatricies : ContentPage
 {
 	List <string> Employees { get; set; }
 	List <string> Positions { get; set; }
+    List<string> EmployeeTraining { get; set; }
+    List<string> PositionTraining { get; set; }
     public TrainingMatricies()
 	{
 		InitializeComponent();
@@ -49,6 +52,7 @@ public partial class TrainingMatricies : ContentPage
         {
             ConnectionFail.IsVisible = true;
         }
+
         Employees = employees;
     }
 
@@ -81,6 +85,74 @@ public partial class TrainingMatricies : ContentPage
         Positions = positions;
     }
 
+    private void getEmployeeTraining(string employeeName)
+    {
+        string query = "SELECT CertificationName FROM Certifications " +
+            "LEFT JOIN CertificationTypes ON Certifications.CertificationID = CertificationTypes.CertificationId " +
+            "LEFT JOIN Employees on Certifications.EmployeeID = Employees.EmployeeID " +
+            "WHERE (Employees.EmployeeFirstName + ' ' + Employees.EmployeeLastName) = '" + employeeName + "';";
+        List<string> employeeTraining = new List<string>();
+
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(Preferences.Default.Get("DBConn", "Not Found")))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string certName = reader.GetString(0);
+                            employeeTraining.Add(certName);
+                        }
+                    }
+                }
+            }
+        }
+        catch
+        {
+            ConnectionFail.IsVisible = true;
+        }
+
+        EmployeeTraining = employeeTraining;
+    }
+
+    private void getPositionTraining(string positionName)
+    {
+        string query = "SELECT CertificationName FROM CertificationPositionMap " +
+            "LEFT JOIN CertificationTypes ON CertificationPositionMap.CertificationID = CertificationTypes.CertificationID " +
+            "LEFT JOIN Positions ON CertificationPositionMap.PositionID = Positions.PositionID " +
+            "WHERE Positions.PositionName = '" + positionName + "';";
+        List<string> positionTraining = new List<string>();
+
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(Preferences.Default.Get("DBConn", "Not Found")))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string certName = reader.GetString(0);
+                            positionTraining.Add(certName);
+                        }
+                    }
+                }
+            }
+        }
+        catch
+        {
+            ConnectionFail.IsVisible = true;
+        }
+
+        PositionTraining = positionTraining;
+    }
+
     private void employeeDeselectBtn_Clicked(object sender, EventArgs e)
     {
         employeePicker.SelectedItem = null;
@@ -96,5 +168,39 @@ public partial class TrainingMatricies : ContentPage
         // generate the training matrix
         // position training across the top
         // empl training across the side
+
+        if (employeePicker.SelectedItem != null && positionPicker.SelectedItem != null)
+        {
+            // show what training the employee has compared to the position
+            getEmployeeTraining(employeePicker.SelectedItem.ToString());
+            getPositionTraining(positionPicker.SelectedItem.ToString());
+        }
+        else if (employeePicker.SelectedItem != null)
+        {
+            // show vs the position they have
+            // this means position needs to be added to the employee table
+        }
+        else if (positionPicker.SelectedItem != null)
+        {
+            // show the required training for the position
+            getPositionTraining(positionPicker.SelectedItem.ToString());
+        }
+        
+
+        addCertLabels(PositionTraining.Count());
+    }
+
+    private void addCertLabels(int totalCerts)
+    {
+        headerStackLayout.Children.Clear();
+
+        for (int i = 0; i < totalCerts; i++)
+        {
+            headerStackLayout.Children.Add(new Label
+            {
+                Text = PositionTraining[i].ToString(),
+                HorizontalOptions = LayoutOptions.StartAndExpand
+            });
+        }
     }
 }
